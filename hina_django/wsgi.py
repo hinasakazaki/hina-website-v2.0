50265040201ce410e0b873d8df780accf5bc6eb1
hinasakazaki@gmail.com <hinasakazaki@gmail.com>
2015-03-21 14:48:25 -0700

    putting os.environ stuff before everything else in wsgi

--- hina_django/wsgi.py
+++ hina_django/wsgi.py
@@ -6,6 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'hina_django.settings'
+
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
@@ -13,8 +16,7 @@ application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
 
 #OLD
-import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'hina_django.settings'
+
 
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hina_django.settings")
 
